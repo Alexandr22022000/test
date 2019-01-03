0820e3ca0cd07972fdc1950522197bf4d067e343
Alex <zol22222@mail.ru>
2019-01-03 18:48:58 +0700

    Add machin log

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express'),
+    bodyParser = require('body-parser'),
     app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const data = process.env;
 
 app.set('port', (process.env.PORT || 4000));
 
 app.get('/*', (req, res) => {
-    res.end("Hello!");
+    res.status(200).json(data);
 });
 
 app.listen(app.get('port'), () => console.log("Server is started on port " + app.get('port')));
